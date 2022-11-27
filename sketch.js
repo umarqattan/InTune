2faa328e3a1f429d93cb04c0be11dc1251b2c950
Umar Qattan <u.qattan@gmail.com>
2022-11-26 22:33:29 -0700

    Add toggle for flat to sharp

--- sketch.js
+++ sketch.js
@@ -47,15 +47,18 @@ function getPitch() {
   })
 }
 
+function setFlat() {
+    notes.isFlat = !notes.isFlat;
+}
+
 class Notes {
 
     constructor() {
-  
+      this.isFlat = false;
       this.notesMap = new Map();
-      this.frequencyMap = new Map();
       this.noteFreqMap = new Map();
-      this.numberOfSemitones = 7*9 + 9*5;
-      
+      this.numberOfSemitones = 7 * 9 + 9 * 5;
+  
       var current = 0;
       var currentNote = '';
       var currentOctave = 0;
@@ -75,34 +78,55 @@ class Notes {
           currentNote = notes[current];
           current = (current + 1) % notes.length;
         }
-      
+  
         if (currentNote === 'C') {
           currentOctave += 1;
         }
-      
+  
         previousNote = `${currentNote}${currentOctave}`;
         this.notesMap[`${i}`] = previousNote;
-      
-        this.frequencyMap[previousNote] = [currentFrequency, currentFrequency * Math.pow(2, 1.0 / 12.0) * 0.9999];
+  
         this.noteFreqMap[previousNote] = currentFrequency;
         currentFrequency = (currentFrequency * Math.pow(2, 1.0 / 12.0));
       }
+      this.noteFreqMap[`${currentNote}${currentOctave}`] = currentFrequency;
+      this.notesMap[this.numberOfSemitones] = `${currentNote}${currentOctave}`;
     }
   
-    getNoteForFrequency(frequency) {
-      return this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)];
-    }
+    // getNoteForFrequency(frequency) {
+    //   return this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)];
+    // }
   
     getNumberOfSemitonesFromStartFrequency(frequency) {
-      return Math.ceil(Math.log2(frequency/this.noteFreqMap['A0']));
+      return Math.floor(12 * Math.log2(frequency / this.noteFreqMap['A0']));
     }
   
     getNumberOfCentsFromStartFrequency(frequency) {
-      return ((Math.log2(frequency/this.noteFreqMap['A0'])) * 10) % 10 / 10;
+      return ((12 * Math.log2(frequency / this.noteFreqMap['A0'])) * 10) % 10 / 10;
     }
   
     getNoteDescription(frequency) {
-      return `You are ${Math.round(this.getNumberOfCentsFromStartFrequency(frequency) * 100)}% from ${this.getNoteForFrequency(frequency)
-    }`
+      return `You are ${Math.round(this.getNumberOfCentsFromStartFrequency(frequency) * 100)}% from ${this.getNoteForFrequency(frequency)}`
+    }
+  
+    getNoteForFrequency(frequency) {
+      let note = this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)];
+      if (!this.isFlat) {
+        return note;
+      } else {
+        if (note.includes('A#')) {
+          return note.replace('A#', 'B♭');
+        } else if (note.includes('C#')) {
+          return note.replace('C#', 'D♭');
+        } else if (note.includes('D#')) {
+          return note.replace('D#', 'E♭');
+        } else if (note.includes('F#')) {
+          return note.replace('F#', 'G♭');
+        } else if (note.includes('G#')) {
+          return note.replace('G#', 'A♭');
+        } else {
+          return note;
+        }
+      }
+    }
   }
-}
