914f6e3b2ddeafa1df3ca9a0552ea79fa7455933
Umar Qattan <u.qattan@gmail.com>
2022-11-27 16:56:52 -0700

    Add vexflow library to show music staff

--- sketch.js
+++ sketch.js
@@ -8,6 +8,34 @@ ml5 Example
 Basic Pitch Detection
 === */
 
+
+const {
+    Renderer,
+    Stave,
+    StaveNote,
+    Voice,
+    Formatter
+  } = Vex.Flow;
+  
+  // Create an SVG renderer and attach it to the DIV element named "boo".
+  const div = document.getElementById('output');
+  const renderer = new Renderer(div, Renderer.Backends.SVG);
+  
+  // Configure the rendering context.
+  renderer.resize(500, 500);
+  const context = renderer.getContext();
+  
+  // Create a stave of width 400 at position 10, 40 on the canvas.
+  const stave = new Stave(10, 40, 150);
+  
+  // Add a clef and time signature.
+  stave.addClef('treble').addTimeSignature('4/4');
+  
+  // Connect it to the rendering context and draw!
+  stave.setContext(context).draw();
+  
+
+
 let audioContext;
 let mic;
 let pitch;
@@ -31,6 +59,10 @@ function modelLoaded() {
   getPitch();
 }
 
+function setFlat() { 
+    notes.isFlat = !notes.isFlat;
+}
+
 function getPitch() {
   if (!initializedNotes) { 
     notes = new Notes();
@@ -38,8 +70,32 @@ function getPitch() {
   }
   pitch.getPitch(function(err, frequency) {
     if (frequency) {
-      select('#result').html(frequency);
-      select('#note').html(notes.getNoteDescription(frequency));
+        select('#result').html(frequency);
+        select('#note').html(notes.getNoteDescription(frequency));
+
+        // Create the notes
+        var notes_ = [
+        // A quarter-note C.
+        new StaveNote({
+            keys: [`${notes.getNoteForFrequency(frequency)}`],
+            duration: 'q'
+        }),
+  
+    ];
+  
+    // Create a voice in 4/4 and add above notes
+    const voice = new Voice({
+        num_beats: 1,
+        beat_value: 4
+    });
+    voice.addTickables(notes_);
+  
+    // Format and justify the notes to 150 pixels.
+    new Formatter().joinVoices([voice]).format([voice], 150);
+  
+    // Render voice
+    voice.draw(context, stave);
+
     } else {
       select('#result').html('No pitch detected');
     }
@@ -47,86 +103,54 @@ function getPitch() {
   })
 }
 
-function setFlat() {
-    notes.isFlat = !notes.isFlat;
-}
-
 class Notes {
-
     constructor() {
-      this.isFlat = false;
-      this.notesMap = new Map();
+      var notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+      var flatNotes = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
+      this.numberOfOctaves = 9;
+      this.currentOctave = 0;
+      this.currentFlatOctave = 0;
+      this.currentNote = '';
+      this.currentFlatNote = '';
+      this.currentFrequency = 27.5;
       this.noteFreqMap = new Map();
-      this.numberOfSemitones = 7 * 9 + 9 * 5;
+      this.noteFreqMapFlat = new Map();
+      this.notesMap = new Map();
+      this.isFlat = false;
   
-      var current = 0;
-      var currentNote = '';
-      var currentOctave = 0;
-      var notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-      var previousNote = '';
-      var currentFrequency = 27.5;
-      for (var i = 0; i < this.numberOfSemitones; i++) {
-        if (previousNote.length === 0) {
-          currentNote = notes[current];
-          current = (current + 1) % notes.length;
-        } else if (previousNote.includes('B') || previousNote.includes('E') || previousNote.includes('#')) {
-          currentNote = notes[current];
-          current = (current + 1) % notes.length;
-        } else if (currentNote.includes('#') == false) {
-          currentNote = `${currentNote}#`;
-        } else {
-          currentNote = notes[current];
-          current = (current + 1) % notes.length;
+      for (var i = 0; i < 109; i++) {
+        this.currentNote = notes[i % notes.length];
+        this.currentFlatNote = flatNotes[i % flatNotes.length];
+        if (this.currentNote === 'C') {
+          this.currentOctave += 1;
+        }
+        if (this.currentFlatNote === 'C') {
+          this.currentFlatOctave += 1;
         }
   
-        if (currentNote === 'C') {
-          currentOctave += 1;
-        }
-  
-        previousNote = `${currentNote}${currentOctave}`;
-        this.notesMap[`${i}`] = previousNote;
-  
-        this.noteFreqMap[previousNote] = currentFrequency;
-        currentFrequency = (currentFrequency * Math.pow(2, 1.0 / 12.0));
+        this.noteFreqMap[`${this.currentNote}/${this.currentOctave}`] = this.currentFrequency;
+        this.noteFreqMapFlat[`${this.currentFlatNote}/${this.currentOctave}`] = this.currentFrequency;
+        console.log(`${this.currentNote}/${this.currentOctave}: ${this.currentFrequency}`);
+        console.log(`${this.currentFlatNote}/${this.currentFlatOctave}: ${this.currentFrequency}`);
+        this.notesMap[`${i}`] = [`${this.currentFlatNote}/${this.currentFlatOctave}`, `${this.currentNote}/${this.currentOctave}`];
+        this.currentFrequency = (this.currentFrequency * Math.pow(2, 1.0 / 12.0));
       }
-      this.noteFreqMap[`${currentNote}${currentOctave}`] = currentFrequency;
-      this.notesMap[this.numberOfSemitones] = `${currentNote}${currentOctave}`;
     }
   
-    // getNoteForFrequency(frequency) {
-    //   return this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)];
-    // }
+    getNoteForFrequency(frequency) {
+      let index = this.isFlat ? 0 : 1;
+      return this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)][index];
+    }
   
     getNumberOfSemitonesFromStartFrequency(frequency) {
-      return Math.floor(12 * Math.log2(frequency / this.noteFreqMap['A0']));
+      return Math.floor(12 * Math.log2(frequency / this.noteFreqMap['A/0']));
     }
   
     getNumberOfCentsFromStartFrequency(frequency) {
-      return ((12 * Math.log2(frequency / this.noteFreqMap['A0'])) * 10) % 10 / 10;
+      return ((12 * Math.log2(frequency / this.noteFreqMap['A/0'])) * 10) % 10 / 10;
     }
   
     getNoteDescription(frequency) {
       return `You are ${Math.round(this.getNumberOfCentsFromStartFrequency(frequency) * 100)}% from ${this.getNoteForFrequency(frequency)}`
-    }
-  
-    getNoteForFrequency(frequency) {
-      let note = this.notesMap[this.getNumberOfSemitonesFromStartFrequency(frequency)];
-      if (!this.isFlat) {
-        return note;
-      } else {
-        if (note.includes('A#')) {
-          return note.replace('A#', 'B♭');
-        } else if (note.includes('C#')) {
-          return note.replace('C#', 'D♭');
-        } else if (note.includes('D#')) {
-          return note.replace('D#', 'E♭');
-        } else if (note.includes('F#')) {
-          return note.replace('F#', 'G♭');
-        } else if (note.includes('G#')) {
-          return note.replace('G#', 'A♭');
-        } else {
-          return note;
-        }
-      }
     }
   }
